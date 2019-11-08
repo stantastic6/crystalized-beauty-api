97e2cb99705dbba357a187bb3ef4af4425d9e043
Stanley Jackson <stanjackson6@gmail.com>
2019-11-07 23:42:03 -0800

    Failover to local mongoDB

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,7 @@
 import { connect } from 'mongoose'
 
 export const connectToDb = async (options?: {}) => {
-  const url: string = process.env.DB_URL!
+  const url: string = process.env.DB_URL! || 'mongodb://localhost:27017'
 
   return await connect(
     url,
